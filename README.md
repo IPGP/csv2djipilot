51be4c06cb19bf35c943919059c1032269e44be1
bonaime <bonaime@ipgp.fr>
2021-07-21 18:43:24 +0200

    speed, onfinish, actions turnmode

--- README.md
+++ README.md
@@ -1,13 +1,31 @@
 csv2djipilot creates a DJI kml file to be imported in a DJI Pilot waypoints.
 Csvfile contains
 ```
-point_name;lon;lat;height;heading;gimbal
-waypoint_A;2.2595689652949886;48.02450015337408;15.0;180;-30
-waypoint_B;2.2596269016191606;48.02450001605067;22.98;180;-50
-waypoint_C;2.2596653502913546;48.02450001248549;25.98;180;-60
+point_name;lon;lat;height;heading;gimbal;speed;turnmode;actions_sequence
+waypoint_A;2.2595689652949886;48.02450015337408;15.0;180;-30;5;1000.SHOOT.1000
+waypoint_B;2.2596269016191606;48.02450001605067;22.98;180;-50;5;1000.REC
+waypoint_C;2.2596653502913546;48.02450001248549;25.98;180;-60;5;STOPREC.2000.G40.SHOOT
 ```
+speed between 0 to 15 m/s
+turnmode is AUTO, C for clockwise or CC for counter clockwise
+actions sequence is a list of actions separted by points without space. There could be no action, one or multiple actions.
+H1000 => Hover 1000ms = 1s
+SHOOT => take a picture 
+G40 =>Gimbal a -40°
+REC => start video recording
+STOPREC => stop video recording
+A-170 => turn aircraft to -170
 
-usage: csv2djipilot.py [-h] [-o OUTPUT] csvfile
+usage: csv2djipilot.py [-h] [-o OUTPUT] [--onfinish {hover,gohome}] csvfile
+
+positional arguments:
+  csvfile               Specify csv input file
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -o OUTPUT, --output OUTPUT
+                        Specify output file (default:stdout)
+  --onfinish {hover,gohome}
 
 ex: 
 ````
@@ -15,7 +33,3 @@ python3 csv2djipilot.py csvfile.csv -o output.kml
 ````
 
 tested with DJI Matrice 210 RTK V2
-
-To do :
-- [ ] User interface to change actions for the waypoints or colums for actions in the csv
-- [ ] Speed for each waypoint in the csv. It is the speed between this waypoint and the next waypoint with range [0, 15] m/s.
--- csv2djipilot.py
+++ csv2djipilot.py
@@ -5,21 +5,32 @@ import csv
 import sys
 import argparse
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument('csvfile', type=argparse.FileType('r'),
                     help="Specify csv input file")
 #parser.add_argument('-outputfile',type=string, required=False, default="pilot.kml")
 parser.add_argument('-o', '--output', type=argparse.FileType('w'),
                     default=sys.stdout, help="Specify output file (default:stdout)")
-
+parser.add_argument('--onfinish', default='hover',
+                    choices=['hover', 'gohome'],
+                    help='Aircraft action when finish. hover or gohome (default: %(default)s)'
+                    )
 args = parser.parse_args()
+
+if args.onfinish == 'hover':
+    ON_FINISH = "Hover"
+elif args.onfinish == 'gohome':
+    ON_FINISH = "GoHome"
+else:
+    sys.exit('onfinish shoud be hover or gohome')
 
 CsvFile = args.csvfile.name
 
 print(f'{CsvFile} to {args.output.name}')
 #CsvFile = 'exemple.csv'
 #CsvFile = 'exemple_simple.csv'
-csv_header = False
+CSV_HEADER = False
 
 XML_string = """<?xml version="1.0" encoding="UTF-8"?>
 
@@ -46,17 +57,17 @@ XML_string = """<?xml version="1.0" encoding="UTF-8"?>
     </Style>
     <Folder>
       <name>Waypoints</name>
-      <description>Waypoints in the Mission.</description>"""
-name = None
-lon = None
-lat = None
-height = None
-heading = None
-gimbal = None
+      <description>Waypoints in the Mission.</description>\n"""
+#name = None
+#lon = None
+#lat = None
+#height = None
+#heading = None
+##gimbal = None
 all_coordinates = ""
 waypoint_number = 1
 
-waypoint = Template("""      <Placemark>
+waypoint_start = Template("""      <Placemark>
         <name>Waypoint$waypoint_number</name>
         <visibility>1</visibility>
         <description>Waypoint</description>
@@ -64,23 +75,37 @@ waypoint = Template("""      <Placemark>
         <ExtendedData xmlns:mis="www.dji.com">
           <mis:useWaylineAltitude>false</mis:useWaylineAltitude>
           <mis:heading>$heading</mis:heading>
-          <mis:turnMode>Auto</mis:turnMode>
+          <mis:turnMode>$turnmode</mis:turnMode>
           <mis:gimbalPitch>$gimbal</mis:gimbalPitch>
-          <mis:useWaylineSpeed>true</mis:useWaylineSpeed>
-          <mis:speed>5.0</mis:speed>
+          <mis:useWaylineSpeed>false</mis:useWaylineSpeed>
+          <mis:speed>$speed</mis:speed>
           <mis:useWaylineHeadingMode>true</mis:useWaylineHeadingMode>
           <mis:useWaylinePointType>true</mis:useWaylinePointType>
           <mis:pointType>LineStop</mis:pointType>
-          <mis:cornerRadius>0.2</mis:cornerRadius>
-          <mis:actions param="1000" accuracy="0" cameraIndex="0" payloadType="0" payloadIndex="0">Hovering</mis:actions>
-          <mis:actions param="0" accuracy="0" cameraIndex="0" payloadType="0" payloadIndex="0">ShootPhoto</mis:actions>
-          <mis:actions param="1000" accuracy="0" cameraIndex="0" payloadType="0" payloadIndex="0">Hovering</mis:actions>
+          <mis:cornerRadius>0.2</mis:cornerRadius>""")
+
+
+waypoint_end = Template("""
         </ExtendedData>
         <Point>
           <altitudeMode>relativeToGround</altitudeMode>
           <coordinates>$lon,$lat,$height</coordinates>
         </Point>
       </Placemark>""")
+hover_template = Template("""
+          <mis:actions param="$length" accuracy="0" cameraIndex="0" payloadType="0" payloadIndex="0">Hovering</mis:actions>""")
+shoot_template = Template("""
+          <mis:actions param="0" accuracy="0" cameraIndex="0" payloadType="0" payloadIndex="0">ShootPhoto</mis:actions>""")
+
+gimbal_template = Template("""
+          <mis:actions param="$gimbal_angle" accuracy="1" cameraIndex="0" payloadType="0" payloadIndex="0">GimbalPitch</mis:actions>""")
+aircraftyaw_template = Template("""
+          <mis:actions param="$aircraftyaw" accuracy="0" cameraIndex="0" payloadType="0" payloadIndex="0">AircraftYaw</mis:actions>""")
+record_template = Template("""
+          <mis:actions param="0" accuracy="0" cameraIndex="0" payloadType="0" payloadIndex="0">StartRecording</mis:actions>""")
+stoprecord_template = Template("""
+          <mis:actions param="0" accuracy="0" cameraIndex="0" payloadType="0" payloadIndex="0">StopRecording</mis:actions>""")
+
 
 all_coordinates_template = Template("$lon,$lat,$height")
 xml_end = Template("""    </Folder>
@@ -91,7 +116,7 @@ xml_end = Template("""    </Folder>
       <ExtendedData xmlns:mis="www.dji.com">
         <mis:altitude>50.0</mis:altitude>
         <mis:autoFlightSpeed>5.0</mis:autoFlightSpeed>
-        <mis:actionOnFinish>GoHome</mis:actionOnFinish>
+        <mis:actionOnFinish>$ON_FINISH</mis:actionOnFinish>
         <mis:headingMode>UsePointSetting</mis:headingMode>
         <mis:gimbalPitchMode>UsePointSetting</mis:gimbalPitchMode>
         <mis:powerSaveMode>false</mis:powerSaveMode>
@@ -120,36 +145,87 @@ xml_end = Template("""    </Folder>
 with open(CsvFile, newline='') as csvfile:
     if csv.Sniffer().has_header(csvfile.read(1024)):
         #print("Header detected !")
-        csv_header = True
+        CSV_HEADER = True
     csvfile.seek(0)
     dialect = csv.Sniffer().sniff(csvfile.read(1024))
     csvfile.seek(0)
     csv_lines = csv.reader(csvfile, dialect)
-    if csv_header:
+    if CSV_HEADER:
         next(csv_lines, None)  # skip the headers
 
     for row in csv_lines:
         if row:
+            print(row)
             name = row[0]
             lon = row[1]
             lat = row[2]
             if lon[0] == '_':
-              lon=lon[1:]
+                lon = lon[1:]
             if lat[0] == '_':
-              lon=lat[1:]
+                lon = lat[1:]
             height = row[3]
             heading = row[4]
             gimbal = row[5]
+            speed = row[6]
+            turnmode = row[7]
+            actions_sequence = row[8]
 
-            XML_string += waypoint.substitute(lon=lon, lat=lat, height=height,
-                                              waypoint_number=waypoint_number, heading=heading, gimbal=gimbal)+"\n"
+            if (float(speed) > 15) or (float(speed) <= 0):
+                sys.exit('speed should be >0 or <=15 m/s for {}'.format(name))
+            if '.' not in speed:
+                speed = speed+'.0'
+
+            if '.' not in gimbal:
+                gimbal = gimbal+'.0'
+
+            if turnmode == 'AUTO':
+                turnmode = 'Auto'
+            elif turnmode == 'C':
+                turnmode = 'Clockwise'
+            elif turnmode == 'CC':
+                turnmode = 'Counterclockwise'
+            else:
+                sys.exit('turnmode shoud be AUTO C or CC for {}'.format(name))
+
+            XML_string += waypoint_start.substitute(
+                turnmode=turnmode, waypoint_number=waypoint_number, speed=speed, heading=heading, gimbal=gimbal)
+
+            # Actions decoding
+            if actions_sequence:
+                action_list = actions_sequence.split('.')
+                for action in action_list:
+                    if action == 'SHOOT':
+                        XML_string += shoot_template.substitute()
+                    elif action == 'REC':
+                        XML_string += record_template.substitute()
+                    elif action == 'STOPREC':
+                        XML_string += stoprecord_template.substitute()
+                    # Gimbal orientation
+                    elif action[0] == 'G':
+                        XML_string += gimbal_template.substitute(
+                            gimbal_angle=action[1:])
+                    # Aircraft orientation
+                    elif action[0] == 'A':
+                        XML_string += aircraftyaw_template.substitute(
+                            aircraftyaw=action[1:])
+                    elif action[0] == 'H':
+                        if float(action[1:]) < 500:
+                            print(float(action[1:]))
+                            sys.exit(
+                                'Hover length is in ms and should be >500  for {}'.format(name))
+                        XML_string += hover_template.substitute(
+                            length=action[1:])
+
+            XML_string += "\n" + \
+                waypoint_end.substitute(lon=lon, lat=lat, height=height,)+"\n"
+
             all_coordinates += all_coordinates_template.substitute(
                 lon=lon, lat=lat, height=height)+" "
         waypoint_number += 1
 # remove last space from coordinates string
 all_coordinates = all_coordinates[:-1]
-XML_string += xml_end.substitute(all_coordinates=all_coordinates)
-
+XML_string += xml_end.substitute(all_coordinates=all_coordinates,
+                                 ON_FINISH=ON_FINISH)
 
 with args.output as outpufile:
     outpufile.write(XML_string)
